86dc460f0c8ba9424e44fc9b3d64690d993292a9
Moses <moses.kumie@outlook.com>
2022-12-20 16:39:35 +1000

    first commit

---
+++ src/PostPage.js
@@ -0,0 +1,44 @@
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { useStoreState, useStoreActions } from "easy-peasy";
+
+const PostPage = () => {
+
+    const { id } = useParams()
+    const deletePost = useStoreActions((actions) => actions.deletePost)
+    const getPostById = useStoreState((state) => state.getPostById)
+    const post = getPostById(id)
+
+    const navigate = useNavigate()
+
+    const handleDelete = (id) => {
+        deletePost(id)
+        navigate('/')
+    }
+
+  return (
+    <main className="PostPage">
+        <article className="post">
+            {post &&
+                <>
+                    <h2>{post.title}</h2>
+                    <p className="postDate">{post.datetime}</p>
+                    <p className="postBody">{post.body}</p>
+                    <Link to={`/edit/${post.id}`}><button className="editButton">Edit Post</button></Link>
+                    <button className="deleteButton" onClick={() => handleDelete(post.id)}>Delete Post</button>
+                </>
+            }
+            {!post &&
+                <>
+                    <h2>Post Not Found</h2>
+                    <p>Well, thats disappointing.</p>
+                    <p>
+                        <Link to='/'>Visit Our Homepage</Link>
+                    </p>
+                </>
+            }
+        </article>
+    </main>
+  )
+};
+
+export default PostPage;
